3e95a62191b1625c9fc186fc8aa2ba3c4c427f14
Shubham <shubham.kedia2010@gmail.com>
2014-02-13 00:40:47 +0530

    form validation

--- app/models/page.rb
+++ app/models/page.rb
@@ -5,6 +5,13 @@ class Page < ActiveRecord::Base
   has_and_belongs_to_many :editors, :class_name =>"AdminUser"
   has_many :sections
   
+  validates_presence_of :name 
+  validates_length_of :name , :maximum => 255
+  validates_presence_of :permalink 
+  validates_length_of :permalink , :maximum => 3..255
+  validates_uniqueness_of :name , :maximum => 255
+
+
    scope :visible, lambda {where(:visible => true)}
    scope :invisible, lambda {where(:visible => false)}
    scope :sorted, lambda {order("pages.position ASC")}
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,8 +2,15 @@ class Subject < ActiveRecord::Base
    # attr_accessible :name, :position, :visible
 
    has_many :pages
-   
+  
+   # 
+   # ids, foriegn key, timstamps, booleans, counters
    validates_presence_of :name 
+   validates_length_of :name , :maximum => 255
+   # diff error msg "cant be blank" or "is to short"
+   # 
+  
+
 
    scope :visible, lambda {where(:visible => true)}
    scope :invisible, lambda {where(:visible => false)}
