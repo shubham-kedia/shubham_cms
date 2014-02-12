c17b5ee7155c46c3a412059019e78ed797ab60e7
Shubham <shubham.kedia2010@gmail.com>
2014-02-13 02:50:42 +0530

    login logout errors removed

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -15,7 +15,7 @@ class AccessController < ApplicationController
 	  		found_user = AdminUser.where(:username => params[:username]).first
 	  		if found_user.authenticate(params[:password])
 	  			flash[:notice] = "You are Now logged in."
-	  			redirect_to('index')
+	  			redirect_to(:action => 'index', :name => found_user.username)
 	  		else
 	  			flash[:notice] = "invalid username/password combination"
 	  			redirect_to(:action => 'login')
