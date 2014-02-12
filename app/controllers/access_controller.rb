a2fa84eeecf0df07f0c608cf88c56f125bd17141
Shubham <shubham.kedia2010@gmail.com>
2014-02-13 03:33:15 +0530

    ession cookie some errors

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -2,6 +2,8 @@ class AccessController < ApplicationController
   
   layout 'admin'
 
+  before_action :confirm_logged_in, :except => [:login, :attempt_login, :logout]
+  
   def index
   	# display text & links
   end
@@ -14,8 +16,12 @@ class AccessController < ApplicationController
 	  	if params[:username].present? && params[:password].present?
 	  		found_user = AdminUser.where(:username => params[:username]).first
 	  		if found_user.authenticate(params[:password])
+	  			# mark user as logged in 
+	  			session[:user_id] = found_user.id
+	  			session[:username] = found_user.username
 	  			flash[:notice] = "You are Now logged in."
-	  			redirect_to(:action => 'index', :name => found_user.username)
+	  			redirect_to(:action => 'index')
+	  			# redirect_to(:action => 'index', :name => found_user.username)
 	  		else
 	  			flash[:notice] = "invalid username/password combination"
 	  			redirect_to(:action => 'login')
@@ -28,7 +34,21 @@ class AccessController < ApplicationController
   end
 
   def logout
+  	# marked user as logout
+  	session[:user_id] = nil
+	session[:username] = nil
   	flash[:notice] = "You are Now logged out."
 	redirect_to(:action => 'login')
   end
+ 
+  def confirm_logged_in
+  	unless session[:user_id]
+  		flash[:notice] = "Please log in."
+  		redirect_to(:action => 'login')
+  		return false
+  	else
+  		return true
+  	end
+  end
+
 end
