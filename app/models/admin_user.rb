0da1591b1f969f6b62139375578ae0f13169dd29
Shubham <shubham.kedia2010@gmail.com>
2014-02-13 01:01:48 +0530

    validate attributes

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -7,18 +7,31 @@ class AdminUser < ActiveRecord::Base
   has_many :section_edits
   has_many :sections, :through => :section_edits
 
-  EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
-  validates_presence_of :first_name
-  validates_length_of :first_name, :maximum => 25
-  validates_presence_of :last_name
-  validates_length_of :last_name, :maximum => 50
-  validates_presence_of :username
-  validates_length_of :username, :within => 8..25
-  validates_uniqueness_of :username
-  validates_presence_of :email
-  validates_uniqueness_of :email
-  validates_length_of :email, :maximum => 100
-  validates_format_of :email, :with => EMAIL_REGEX
-  validates_confirmation_of :email
+  # EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
+  # validates_presence_of :first_name
+  # validates_length_of :first_name, :maximum => 25
+  # validates_presence_of :last_name
+  # validates_length_of :last_name, :maximum => 50
+  # validates_presence_of :username
+  # validates_length_of :username, :within => 8..25
+  # validates_uniqueness_of :username
+  # validates_presence_of :email
+  # validates_uniqueness_of :email
+  # validates_length_of :email, :maximum => 100
+  # validates_format_of :email, :with => EMAIL_REGEX
+  # validates_confirmation_of :email
 
+  # Shortcut validations
+  validates :first_name, :presence => true, 
+                         :length => {:maximum => 25}
+  validates :last_name, :presence => true,
+                         :length => {:maximum => 25}
+  validates :username, :presence => true, 
+                       :length => {:within => 8..25}, 
+                       :uniqueness => true
+  validates :email, :presence => true, 
+                    :length => {:maximum => 100},
+                    :uniqueness => true,
+                    :format => EMAIL_REGEX,
+                    :confirmation => true
 end
