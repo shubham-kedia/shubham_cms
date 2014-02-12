0b9abee2664e5c209fee972745d0bd01d48297d8
Shubham <shubham.kedia2010@gmail.com>
2014-02-13 01:42:34 +0530

    data validatio-encrypted password

--- db/schema.rb
+++ db/schema.rb
@@ -9,70 +9,69 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140208011822) do
+ActiveRecord::Schema.define(version: 20140212195808) do
 
-  create_table "admin_users", :force => true do |t|
-    t.string   "first_name",      :limit => 25
-    t.string   "last_name",       :limit => 50
-    t.string   "email",           :limit => 100, :default => "", :null => false
-    t.string   "username",        :limit => 25
-    t.string   "hashed_password", :limit => 40
-    t.datetime "created_at",                                     :null => false
-    t.datetime "updated_at",                                     :null => false
+  create_table "admin_users", force: true do |t|
+    t.string   "first_name",      limit: 25
+    t.string   "last_name",       limit: 50
+    t.string   "email",           limit: 100, default: "", null: false
+    t.string   "username",        limit: 25
+    t.datetime "created_at",                               null: false
+    t.datetime "updated_at",                               null: false
+    t.string   "password_digest"
   end
 
-  add_index "admin_users", ["username"], :name => "index_admin_users_on_username"
+  add_index "admin_users", ["username"], name: "index_admin_users_on_username", using: :btree
 
-  create_table "admin_users_pages", :id => false, :force => true do |t|
+  create_table "admin_users_pages", id: false, force: true do |t|
     t.integer "admin_user_id"
     t.integer "page_id"
   end
 
-  add_index "admin_users_pages", ["admin_user_id", "page_id"], :name => "index_admin_users_pages_on_admin_user_id_and_page_id"
+  add_index "admin_users_pages", ["admin_user_id", "page_id"], name: "index_admin_users_pages_on_admin_user_id_and_page_id", using: :btree
 
-  create_table "pages", :force => true do |t|
+  create_table "pages", force: true do |t|
     t.integer  "subject_id"
     t.string   "name"
     t.string   "permalink"
     t.integer  "position"
-    t.boolean  "visible",    :default => false
-    t.datetime "created_at",                    :null => false
-    t.datetime "updated_at",                    :null => false
+    t.boolean  "visible",    default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
-  add_index "pages", ["permalink"], :name => "index_pages_on_permalink"
-  add_index "pages", ["subject_id"], :name => "index_pages_on_subject_id"
+  add_index "pages", ["permalink"], name: "index_pages_on_permalink", using: :btree
+  add_index "pages", ["subject_id"], name: "index_pages_on_subject_id", using: :btree
 
-  create_table "section_edits", :force => true do |t|
+  create_table "section_edits", force: true do |t|
     t.integer  "admin_user_id"
     t.integer  "section_id"
     t.string   "summary"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
-  create_table "sections", :force => true do |t|
+  create_table "sections", force: true do |t|
     t.integer  "page_id"
     t.string   "name"
-    t.string   "permalink"
     t.integer  "position"
-    t.boolean  "visible",      :default => false
+    t.boolean  "visible",      default: false
     t.string   "content_type"
     t.text     "content"
-    t.datetime "created_at",                      :null => false
-    t.datetime "updated_at",                      :null => false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
   end
 
-  add_index "sections", ["page_id"], :name => "index_sections_on_page_id"
+  add_index "sections", ["page_id"], name: "index_sections_on_page_id", using: :btree
 
-  create_table "subjects", :force => true do |t|
+  create_table "subjects", force: true do |t|
     t.string   "name"
     t.integer  "position"
-    t.boolean  "visible",    :default => false
-    t.datetime "created_at",                    :null => false
-    t.datetime "updated_at",                    :null => false
+    t.boolean  "visible",    default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
 end
